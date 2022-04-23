8fca4fbdefae78545aa0e5fa2cecbbfa49b1aeb6
Nikita Skrundz <theskrundik@gmail.com>
2022-04-23 14:32:57 +0300

    init commit

---
+++  calculator.js
@@ -0,0 +1,28 @@
+const sum = (firstNumber, secondNumber) => {
+    return firstNumber + secondNumber;
+}
+const deduction = (firstNumber, secondNumber) => {
+    return firstNumber - secondNumber;
+}
+const multiply = (firstNumber, secondNumber) => {
+    return firstNumber * secondNumber;
+}
+const division = (firstNumber, secondNumber) => {
+    return firstNumber / secondNumber
+}
+const exponent = (firstNumber, secondNumber) => {
+    return firstNumber ** secondNumber
+}
+
+const digitOne = 1;
+const digitTwo = 2;
+const digitThree = 3;
+const digitFour = 4;
+const digitFive = 5;
+const digitSix = 6;
+const digitSeven = 7;
+const digitEight = 8;
+const digitNine = 9;
+const digitZero = 0;
+
+console.log(sum(digitFour, digitSeven));
